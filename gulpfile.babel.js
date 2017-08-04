1b021a587dae158cec98380b64743370af34d8d1
yys <yysun2000@nate.com>
2017-08-04 18:46:15 +0900

    첫번째 커밋

---
+++ gulpfile.babel.js
@@ -0,0 +1,11 @@
+import gulp from 'gulp';
+import browsersync from 'browser-sync'
+
+gulp.task("default",()=>{
+  browsersync.init({
+    server:{
+      baseDir:"./"
+    }
+  })
+  gulp.watch("./*.html",browsersync.reload)
+})
